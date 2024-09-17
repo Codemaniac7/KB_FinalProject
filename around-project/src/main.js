1493f940fb33f31b06957176159e01fdddab5ec1
JunSeopKim <wnqtjq8958@naver.com>
2024-09-17 22:32:00 +0900

    fix : import 경로 수정

--- around-project/src/main.js
+++ around-project/src/main.js
@@ -1,6 +1,7 @@
 import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
+// import './assets/styles/main.scss'
 
 // CSS 파일들
 import 'bootstrap/dist/css/bootstrap.min.css';
